0aab79e1d6f4972e909e6b5033f2f0e92cfb6ddb
BalajiUmapathy <118367169+BalajiUmapathy@users.noreply.github.com>
2024-07-22 19:36:16 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Other configuration options
+    output: 'export', // Add this line to enable static export
+  }
+  
+  export default nextConfig;
+  
